28f7e78edc773f7da8317ac571f2475648567c80
Plsr <christian@gigs.com>
2024-07-05 14:57:12 +0200

    Add theme toggle
    First things first

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/ThemeToggle";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <ThemeToggle />
       <Input />
     </main>
   );
