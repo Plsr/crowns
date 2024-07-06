97bf4c986414bd09085b12bcb3a18b68c92d5072
Plsr <christian@gigs.com>
2024-07-06 10:00:11 +0200

    Make conversion easier

--- components/CurrencyConverter.tsx
+++ components/CurrencyConverter.tsx
@@ -1,27 +1,25 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, useRef, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export const CurrencyConverter = () => {
-  const [result, setResult] = useState<undefined | number>(undefined);
+  const [result, setResult] = useState<undefined | string>(undefined);
   const dkkInputRef = useRef<HTMLInputElement | null>(null);
   const eurInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleConvertClick = () => {
-    if (!dkkInputRef.current) {
-      return;
-    }
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
 
-    if (isNaN(parseInt(dkkInputRef.current?.value))) {
+    if (!value) {
       return;
     }
 
     const rate = 0.13;
 
-    setResult(parseInt(dkkInputRef.current.value) * rate);
+    setResult((value * rate).toFixed(2));
   };
 
   return (
@@ -32,14 +30,16 @@ export const CurrencyConverter = () => {
         </CardHeader>
         <CardContent>
           <div className="grid grid-cols-2 gap-4">
-            <Input type="number" placeholder="DKK" ref={dkkInputRef} />
+            <Input
+              type="number"
+              placeholder="DKK"
+              ref={dkkInputRef}
+              onChange={handleInputChange}
+            />
             <Button variant="secondary" disabled>
               {result} €
             </Button>
           </div>
-          <Button className="w-full mt-6" onClick={handleConvertClick}>
-            convert
-          </Button>
         </CardContent>
       </Card>
     </>
